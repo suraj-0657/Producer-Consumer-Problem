2c8842c5119dcddff32cdd0d1b0b617eb372ebb5
Suraj Kumar <surajkumarsonari@gmail.com>
2023-04-09 23:23:23 +0530

    Add files via upload

---
+++ multithreadingProducerConsumer.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <deque>
+//#include <condition_variable>
+
+std::mutex m;
+std::condition_variable cv;
+std::deque<int> buffer;
+unsigned const int maxSize{100};
+
+void producer(int val)
+{
+    while(val) {
+        std::unique_lock<std::mutex> locker(m);
+        cv.wait(locker, [] () { return buffer.size() < maxSize; });
+        buffer.push_back(val);
+        std::cout << "Value Produced: " << val <<"\n";
+        val--;
+        locker.unlock();
+        cv.notify_one();
+    }
+}
+
+void consumer()
+{
+    while(true) {
+        std::unique_lock<std::mutex> locker(m);
+        cv.wait(locker, [] () { return buffer.size() > 0; });
+        std::cout << "Value Consumed: " << buffer.back() <<"\n";
+        buffer.pop_back();
+        locker.unlock();
+        cv.notify_one();
+    }
+}
+
+int main()
+{
+    std::jthread pro{producer, 100};
+    std::jthread cons{consumer};
+    return 0;
+}
